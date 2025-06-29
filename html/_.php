52f1844e40ce988ea044074eb1464e3872807feb
JMDirksen <jeftadirksen@gmail.com>
2025-06-29 15:12:48 +0200

    Database conversions

--- html/_.php
+++ html/_.php
@@ -2,18 +2,18 @@
 require('init.php');
 echo "<pre>";
 
-echo PHP_EOL . "Session" . PHP_EOL;
+echo "[Session]" . PHP_EOL . PHP_EOL;
 foreach ($_SESSION as $key => $value) {
     echo $key . " = " . var_export($value, true) . PHP_EOL;
 }
 
-echo PHP_EOL . "Settings" . PHP_EOL;
+echo PHP_EOL . PHP_EOL . "[Settings]" . PHP_EOL . PHP_EOL;
 $results = DB->query('SELECT * FROM setting');
 while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
     echo $row['name'] . " = " . $row['value'] . PHP_EOL;
 }
 
-echo PHP_EOL . "Users" . PHP_EOL;
+echo PHP_EOL . PHP_EOL . "[Users]" . PHP_EOL . PHP_EOL;
 $results = DB->query('SELECT * FROM user');
 while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
     print_r($row);
--- html/init.php
+++ html/init.php
@@ -15,5 +15,15 @@ define("DB", new SQLite3("../consolegame.db"));
 
 // Database setup
 DB->exec('CREATE TABLE IF NOT EXISTS setting (name TEXT PRIMARY KEY, value TEXT)');
-DB->exec('INSERT OR IGNORE INTO setting (name, value) VALUES ("db_version", "1")');
-DB->exec('CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)');
+DB->exec('INSERT OR IGNORE INTO setting (name, value) VALUES ("db_version", "0")');
+
+// Database conversions
+$results = DB->query('SELECT value FROM setting WHERE name = "db_version"');
+$db_version = $results->fetchArray(SQLITE3_ASSOC)['value'];
+
+// Version 1
+if ($db_version < 1) {
+    DB->exec('CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)');
+    DB->exec('UPDATE setting SET value = "1" WHERE name = "db_version"');
+    $db_version = 1;    
+}
